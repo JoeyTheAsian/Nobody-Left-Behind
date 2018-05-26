97a7da6cd03a78702722df4027b2c5c1777adad7
Joey Tong <joeyptong@yahoo.com>
2018-05-26 15:05:36 +0900

    Sounds

---
+++ Japan Jam/Assets/Scripts/soundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour {
+
+    public AudioSource efxSource;
+
+    // Use this for initialization
+    void Start () {
+        efxSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Random.Range(0, 100) == 0 && efxSource.isPlaying == false) {
+            efxSource.Play();
+        }
+    }
+}
