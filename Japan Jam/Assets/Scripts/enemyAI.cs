94c48426321c80050ad51080cd528c4917327f8d
Joey Tong <joeyptong@yahoo.com>
2018-05-26 15:28:05 +0900

    lighting fixes

--- Japan Jam/Assets/Scripts/enemyAI.cs
+++ Japan Jam/Assets/Scripts/enemyAI.cs
@@ -30,6 +30,7 @@ public class enemyAI : MonoBehaviour {
 		Vector2 tempPos = transform.position;
 		if(CheckDistance()){
 			Seek (target.transform.position);
+            Debug.Log(target.name);
 			//RotateTowardsPlayer ();
 		}
 		else{
