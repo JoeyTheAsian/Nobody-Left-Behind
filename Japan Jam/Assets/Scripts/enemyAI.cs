7fab6b02cba05de4529b15e208e8b7de2e537d14
Nathan Glick <nxg9997@g.rit.edu>
2018-05-26 10:17:05 +0900

    basic control and AI

---
+++ Japan Jam/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour {
+
+	public float maxSpeed;
+	public float maxForce;
+	public Vector2 acceleration;
+	public Vector2 velocity;
+	public Vector2 position;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		player = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		acceleration = Vector2.zero;
+		Vector2 ultimate = Vector2.zero;
+		Vector2 tempPos = transform.position;
+		Seek (player);
+		velocity += acceleration;
+		velocity = Vector2.ClampMagnitude (velocity, maxSpeed);
+		tempPos += velocity;
+		transform.position = tempPos;
+	}
+
+	void Seek(GameObject p){
+		Vector2 result = Vector2.zero;
+		result = p.transform.position - transform.position;
+		result.Normalize ();
+		result *= maxForce;
+		ApplyForce (result);
+	}
+
+	void ApplyForce(Vector2 force){
+		acceleration += force;
+	}
+}
---
+++ Japan Jam/Assets/Scripts/playerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour {
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement(){
+		Vector2 tempPos = transform.position;
+		if (Input.GetKey(KeyCode.A)){
+			tempPos.x -= speed;
+		}
+		if (Input.GetKey(KeyCode.D)){
+			tempPos.x += speed;
+
+		}
+		if (Input.GetKey(KeyCode.W)){
+			tempPos.y += speed;
+
+		}
+		if (Input.GetKey(KeyCode.S)){
+			tempPos.y -= speed;
+
+		}
+		transform.position = tempPos;
+	}
+
+}
