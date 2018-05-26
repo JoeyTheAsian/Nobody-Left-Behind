9be824daff04ebecf3032a8d95afe5150ffec71d
Nathan Glick <nxg9997@g.rit.edu>
2018-05-26 16:25:55 +0900

    randomspawn

--- Japan Jam/Assets/Scripts/Manager.cs
+++ Japan Jam/Assets/Scripts/Manager.cs
@@ -7,6 +7,10 @@ public class Manager : MonoBehaviour {
 	public float boundsHeight;
 	public float boundWidth;
 	public GameObject tree;
+	public GameObject character;
+
+	public float treeNum;
+	public float characterNum;
 
 	private Vector2 topLeft;
 	private float spawnDistance;
@@ -14,7 +18,10 @@ public class Manager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		topLeft = new Vector2 (-boundWidth / 2, boundsHeight / 2);
-		spawnDistance = 7f;
+		spawnDistance = 3f;
+		SpawnBounds ();
+		SpawnObstacles ();
+		SpawnCharacters ();
 	}
 	
 	// Update is called once per frame
@@ -23,8 +30,33 @@ public class Manager : MonoBehaviour {
 	}
 
 	void SpawnBounds(){
-		for (int i = 0; i < boundsHeight / spawnDistance; i+=spawnDistance) {
-			Instantiate (tree, new Vector2 (-boundWidth / 2), 9);
+		for (int i = 0; i < boundsHeight * 2; i+=(int)spawnDistance) {
+			Instantiate (tree, new Vector2 ((-boundWidth / 2), boundsHeight - i), Quaternion.identity);
+		}
+		for (int i = 0; i < boundsHeight * 2; i+=(int)spawnDistance) {
+			Instantiate (tree, new Vector2 ((boundWidth / 2), boundsHeight - i), Quaternion.identity);
+		}
+		for (int i = 0; i < boundWidth; i+=(int)spawnDistance) {
+			Instantiate (tree, new Vector2 (boundWidth / 2 - i, boundsHeight), Quaternion.identity);
+		}
+		for (int i = 0; i < boundWidth; i+=(int)spawnDistance) {
+			Instantiate (tree, new Vector2 (boundWidth / 2 - i, -boundsHeight), Quaternion.identity);
+		}
+	}
+
+	void SpawnObstacles(){
+		for (int i = 0; i < treeNum; i++) {
+			float rX = Random.Range (topLeft.x, topLeft.x + boundWidth);
+			float rY = Random.Range (topLeft.y, topLeft.y + boundsHeight * 2) - boundsHeight * 1.5f;
+			Instantiate (tree, new Vector2 (rX, rY), Quaternion.identity);
+		}
+	}
+
+	void SpawnCharacters(){
+		for (int i = 0; i < characterNum; i++) {
+			float rX = Random.Range (topLeft.x, topLeft.x + boundWidth);
+			float rY = Random.Range (topLeft.y, topLeft.y + boundsHeight * 2) - boundsHeight * 1.5f;
+			Instantiate (character, new Vector2 (rX, rY), Quaternion.identity);
 		}
 	}
 }
