746287cb00e502654bc8c7a4494d59f543dfce00
Nathan Glick <nxg9997@g.rit.edu>
2018-05-26 15:59:33 +0900

    Monster changes

---
+++ Japan Jam/Assets/Scripts/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	public float boundsHeight;
+	public float boundWidth;
+	public GameObject tree;
+
+	private Vector2 topLeft;
+	private float spawnDistance;
+
+	// Use this for initialization
+	void Start () {
+		topLeft = new Vector2 (-boundWidth / 2, boundsHeight / 2);
+		spawnDistance = 7f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnBounds(){
+		for (int i = 0; i < boundsHeight / spawnDistance; i+=spawnDistance) {
+			Instantiate (tree, new Vector2 (-boundWidth / 2), 9);
+		}
+	}
+}
