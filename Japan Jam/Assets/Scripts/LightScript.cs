8f92ef75521e0d2bb98fd369dab0eb541775c134
Nathan Glick <nxg9997@g.rit.edu>
2018-05-26 15:14:31 +0900

    Light

---
+++ Japan Jam/Assets/Scripts/LightScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+	public float intensity;
+	public float range;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ManageLight ();
+	}
+
+	void ManageLight(){
+		int followCount = gameObject.GetComponent<playerControl> ().followers.Count;
+
+		intensity = 1f + followCount;
+		range = 17f + 3f * followCount;
+
+		gameObject.GetComponentInChildren<Light> ().intensity = intensity;
+		gameObject.GetComponentInChildren<Light> ().range = range;
+
+	}
+}
