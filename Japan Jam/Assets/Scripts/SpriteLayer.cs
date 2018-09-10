e32f7bb84fb5ba6589fb7a918de678708c88f41f
Joey Tong <joeyptong@yahoo.com>
2018-09-10 00:01:47 -0400

    Sorting layer bug fix

---
+++ Japan Jam/Assets/Scripts/SpriteLayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLayer : MonoBehaviour {
+    public SpriteRenderer spriteRenderer;
+    public bool tree;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (tree)
+        {
+            spriteRenderer.sortingOrder = -1 * (int)(this.transform.parent.transform.position.y * 100);
+        }
+        else
+        {
+            spriteRenderer.sortingOrder = -1 * (int)(this.transform.position.y * 100);
+        }
+    }
+}
